57ae49017c702f86b2fad6e688db88ec7c5d44fc
Oleh Ivaniuk <fedencer7@gmail.com>
2022-11-24 12:27:14 +0200

    Splitting the 'CreateBountyProposal' page into two parts.

--- ui/src/pages/CreateBountyProposal/CreateBountyProposal.jsx
+++ ui/src/pages/CreateBountyProposal/CreateBountyProposal.jsx
@@ -2,8 +2,10 @@
 /* eslint-disable id-length */
 import { yupResolver } from '@hookform/resolvers/yup';
 import LoadingButton from '@mui/lab/LoadingButton';
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import { useStoreActions, useStoreState } from "easy-peasy";
+import { useState } from "react";
 import { useForm } from 'react-hook-form';
 import { Link } from "react-router-dom";
 import * as Yup from 'yup';
@@ -20,6 +22,7 @@ const CreateBountyProposal = () => {
   const mailto = 'joep@nearweek.com';
   const wallet = useStoreState((state) => state.entities?.wallet);
   const setTemporaryData = useStoreActions((actions) => actions.setTemporaryData);
+  const [createBountyPage, setCreateBountyPage] = useState(false);
 
   const validationSchema = Yup.object().shape({
     bounty_description: Yup.string().required('Bounty description is required'),
@@ -50,6 +53,10 @@ const CreateBountyProposal = () => {
     formState: { errors },
   } = useForm(formOptions);
 
+  const handleContinueButton = () => {
+    setCreateBountyPage(true);
+  };
+
   const handleSubmitButton = (data) => {
     if (
       wallet?.isSignedIn() &&
@@ -77,90 +84,117 @@ const CreateBountyProposal = () => {
         <Box className={classes.bountyBox}>
           <Box display="flex" justifyContent="center">
             <Box className={classes.textBlock}>
-              <Box className={classes.thankYouScreensTitle}>
-                <Box component="h2" sx={{ fontWeight: [400], fontSize: 36, m: 0 }}>
-                  Thank you for filling in the form.<br/>
-                  The form input will be sent to the promotional channels in NEAR&apos;s ecosystem.
-                </Box>
-              </Box>
-              <Box className={classes.thankYouScreensDescription}>
-                <Box component="h2" sx={{ fontWeight: [400], fontSize: 20, m: 0 }}>
-                  If you have any further questions, feel free to reach out to&nbsp;
-                  <Link
-                    to='#'
-                    onClick={(event) => {
-                      event.preventDefault();
-                      window.location.href = `mailto:${mailto}`;
-                    }}
-                  >
-                    {mailto}
-                  </Link>.
-                </Box>
-              </Box>
-              <Box className={classes.thankYouScreensTitle}>
-                <Box component="h2" sx={{ fontWeight: [400], fontSize: 36, m: 0, pt: 3 }}>
-                  Now it is necessary to create an proposal to create a Bounty in the DAO<br/>
-                  (Bond 1 NEAR).
-                </Box>
-              </Box>
-              <Box className={classes.form}>
-                <Box className={classes.inputGroup}>
-                  <Input
-                    id="bounty-description"
-                    label={'Bounty description'}
-                    name="bounty_description"
-                    register={register}
-                    errors={errors}
-                  />
-                </Box>
-                <Box className={classes.inputGroup}>
-                  <Input
-                    id="bounty-amount"
-                    label={'Bounty amount (NEAR)'}
-                    name="bounty_amount"
-                    type="number"
-                    register={register}
-                    errors={errors}
-                  />
-                </Box>
-                <Box className={classes.inputGroup}>
-                  <Input
-                    id="bounty-times"
-                    label={'Available Claims'}
-                    name="bounty_times"
-                    type="number"
-                    register={register}
-                    errors={errors}
-                  />
-                </Box>
-                <Box className={classes.inputGroup}>
-                  <Input
-                    id="bounty-max-deadline"
-                    label={'Days to Complete'}
-                    name="bounty_max_deadline"
-                    type="number"
-                    register={register}
-                    errors={errors}
-                  />
-                </Box>
-              </Box>
-              <Box display="flex" sx={{ paddingRight: 2, paddingLeft: 2, paddingBottom: 3 }}>
-                <LoadingButton
-                  sx={{
-                    borderRadius: 3,
-                    p: '12px 37px',
-                  }}
-                  disabled={isNearLoading}
-                  loading={isNearLoading}
-                  variant="contained"
-                  onClick={handleSubmit(handleSubmitButton)}
-                  disableElevation
-                >
-                  <Box style={{ fontWeight: [700] }}>
-                    Continue
+              {createBountyPage ?
+                <>
+                  <Box className={classes.form}>
+                    <Box className={classes.formHeader}>
+                      <Typography className={classes.formTitle} variant="h6">
+                        Propose to Create a Bounty
+                      </Typography>
+                    </Box>
+                    <Box className={classes.inputGroup}>
+                      <Input
+                        id="bounty-description"
+                        label={'Bounty description'}
+                        name="bounty_description"
+                        register={register}
+                        errors={errors}
+                      />
+                    </Box>
+                    <Box className={classes.inputGroup}>
+                      <Input
+                        id="bounty-amount"
+                        label={'Bounty amount (NEAR)'}
+                        name="bounty_amount"
+                        type="number"
+                        register={register}
+                        errors={errors}
+                      />
+                    </Box>
+                    <Box className={classes.inputGroup}>
+                      <Input
+                        id="bounty-times"
+                        label={'Available Claims'}
+                        name="bounty_times"
+                        type="number"
+                        register={register}
+                        errors={errors}
+                      />
+                    </Box>
+                    <Box className={classes.inputGroup}>
+                      <Input
+                        id="bounty-max-deadline"
+                        label={'Days to Complete'}
+                        name="bounty_max_deadline"
+                        type="number"
+                        register={register}
+                        errors={errors}
+                      />
+                    </Box>
                   </Box>
-                </LoadingButton>
-              </Box>
+                  <Box display="flex" sx={{ paddingRight: 2, paddingLeft: 2, paddingBottom: 3 }}>
+                    <LoadingButton
+                      sx={{
+                        borderRadius: 3,
+                        p: '12px 37px',
+                      }}
+                      disabled={isNearLoading}
+                      loading={isNearLoading}
+                      variant="contained"
+                      onClick={handleSubmit(handleSubmitButton)}
+                      disableElevation
+                    >
+                      <Box style={{ fontWeight: [700] }}>
+                        Submit
+                      </Box>
+                    </LoadingButton>
+                  </Box>
+                </>
+                :
+                <>
+                  <Box className={classes.thankYouScreensTitle}>
+                    <Box component="h2" sx={{ fontWeight: [400], fontSize: 36, m: 0 }}>
+                      Thank you for filling in the form.<br/>
+                      The form input will be sent to the promotional channels in NEAR&apos;s ecosystem.
+                    </Box>
+                  </Box>
+                  <Box className={classes.thankYouScreensDescription}>
+                    <Box component="h2" sx={{ fontWeight: [400], fontSize: 20, m: 0 }}>
+                      If you have any further questions, feel free to reach out to&nbsp;
+                      <Link
+                        to='#'
+                        onClick={(event) => {
+                          event.preventDefault();
+                          window.location.href = `mailto:${mailto}`;
+                        }}
+                      >
+                        {mailto}
+                      </Link>.
+                    </Box>
+                  </Box>
+                  <Box className={classes.thankYouScreensTitle}>
+                    <Box component="h2" sx={{ fontWeight: [400], fontSize: 36, m: 0, pt: 3 }}>
+                      Now it is necessary to create an proposal to create a Bounty in the DAO<br/>
+                      (Bond 1 NEAR).
+                    </Box>
+                  </Box>
+                  <Box display="flex" sx={{ paddingRight: 2, paddingLeft: 2, mt: 4 }}>
+                    <Button
+                      sx={{
+                        borderRadius: 3,
+                        p: '12px 37px',
+                      }}
+                      variant="contained"
+                      onClick={handleContinueButton}
+                      disableElevation
+                    >
+                      <Box style={{ fontWeight: [700] }}>
+                        Continue
+                      </Box>
+                    </Button>
+                  </Box>
+                </>
+              }
             </Box>
           </Box>
         </Box>
